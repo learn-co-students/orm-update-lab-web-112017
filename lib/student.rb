bbcbac70c0e0827e59ba130b641ee01e99f28037
walkingalchemy <walkingalchemy@gmail.com>
2017-11-29 16:07:45 -0500

    Define Student .create_table and .drop_table to create and destroy the students table in DB

--- lib/student.rb
+++ lib/student.rb
@@ -12,6 +12,23 @@ class Student
     @id = id
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+    SQL
+    DB[:conn].execute(sql)
+  end
 
 
 end
